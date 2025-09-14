67a9bcf8086a86d8a3c2fbeb1f316ae54338e0d3
kittenpoker <kittenpokeruk@yahoo.co.uk>
2025-09-14 23:12:52 +0100

    finish bookbot

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 
-
-def main(bookname):
-    book = "books/"+bookname+".txt"
-    book_text = get_book_text(book)
-    print (book_text)
-
+import sys
 from stats import word_count
 from stats import char_count
+from stats import chars_list
 
 
-
-word_count("frankenstein")
-charcount = char_count("frankenstein")
-print (charcount)
+if len(sys.argv) != 2:
+    print ("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:    
+    bookpath = (sys.argv[1])
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {bookpath}...")
+    print ("----------- Word Count -----------")
+    print (f"Found {word_count(bookpath)} total words")
+    print ("--------- Character Count ---------")
+    charslist = chars_list(bookpath)
+    for char in charslist:
+        print (f'{char["char"]}: {char["num"]}')
+    print ("============ END =============")
--- stats.py
+++ stats.py
@@ -1,17 +1,30 @@
-def get_book_text(book):
-    with open(book) as f:
+def get_book_text(bookpath):
+    with open(bookpath) as f:
         return f.read()
 
-def word_count(bookname):
-    book_text = get_book_text("books/"+bookname+".txt")
+def word_count(bookpath):
+    book_text = get_book_text(bookpath)
     words = book_text.split()
-    print(f"{len(words)} words found in the document")
+    #print(f"{len(words)} words found in the document")
+    return (len(words))
 
-def char_count(bookname):
-    book_text = get_book_text("books/"+bookname+".txt")
+def char_count(bookpath):
+    book_text = get_book_text(bookpath)
     chars = {}
     for char in book_text.lower():
         if char not in chars:
             chars[char] = 0
         chars[char] = chars[char] +1
     return (chars)
+
+def sort_on(items):
+    return items["num"]
+
+def chars_list (bookpath):
+    charcount = char_count(bookpath)
+    charslist = []
+    for char, num in charcount.items():
+        if char.isalpha():
+            charslist.append ({"char": char, "num": num})
+    charslist.sort(reverse=True, key=sort_on)
+    return (charslist)
